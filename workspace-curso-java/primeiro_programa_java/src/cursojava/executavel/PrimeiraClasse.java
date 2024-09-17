2b62e4afdf4d7af03af01e863498de4fcd72ae3b
Dan Vasques <dan.vasques@outlook.com.br>
2024-09-17 20:17:10 -0300

    primeiro commit do projeto

---
+++ workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
@@ -0,0 +1,23 @@
+package cursojava.executavel;
+
+public class PrimeiraClasse {
+
+	/* Main é um método auto executal em Java */
+	public static void main(String[] args) {
+
+		int maiorIdade = 18;
+		int idosoIdade = 60;
+		int jovemIdade = 16;
+		int criancaIdade = 7;
+
+		String cpf = "123.456.789-10";
+
+		System.out.println("Maior idade é: " + maiorIdade);
+		System.out.println("Idoso idade é: " + idosoIdade);
+		System.out.println("Jovem idade é: " + jovemIdade);
+		System.out.println("Criança idade é: " + criancaIdade);
+		System.out.println("O CPF é: " + cpf);
+
+	}
+
+}
