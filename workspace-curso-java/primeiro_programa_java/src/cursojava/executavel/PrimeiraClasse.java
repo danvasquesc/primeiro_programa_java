1b8bb8728b9dc9f7b84ff7c8051d0f7de5e9d320
Dan Vasques <dan.vasques@outlook.com.br>
2024-09-17 21:02:46 -0300

    aula sobre variaveis globais e locais

--- workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
+++ workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
@@ -2,21 +2,31 @@ package cursojava.executavel;
 
 public class PrimeiraClasse {
 
-	/* Main é um método auto executal em Java */
+	/* Variável Global é acessivel a todos e seu valor compartilhado */
+	static int maiorIdadeGlobal = 30;
+
+	/* Main é um método auto executavel em Java */
 	public static void main(String[] args) {
 
+		/*
+		 * Variável local porque pertence somente a esse método e o valor fica dentro do
+		 * metodo que pertence
+		 */
 		int maiorIdade = 18;
-		int idosoIdade = 60;
-		int jovemIdade = 16;
-		int criancaIdade = 7;
+		System.out.println("Valor da variavel local = " + maiorIdade);
 
-		String cpf = "123.456.789-10";
+		/* necessita colocar o metodo criado dentro do metodo executavel */
+		metodo2();
 
-		System.out.println("Maior idade é: " + maiorIdade);
-		System.out.println("Idoso idade é: " + idosoIdade);
-		System.out.println("Jovem idade é: " + jovemIdade);
-		System.out.println("Criança idade é: " + criancaIdade);
-		System.out.println("O CPF é: " + cpf);
+	}
+
+	public static void metodo2() {
+
+		/* Variavel local do metodo */
+		int mediaAno = 50;
+
+		/* Aqui vai imprimir a variavel global */
+		System.out.println("Valor da variavel Global = " + maiorIdadeGlobal);
 
 	}
 
