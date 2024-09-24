4e13492212517125f2f18fabc54ecc0b6d86c85a
Dan Vasques <dan.vasques@outlook.com.br>
2024-09-24 19:36:27 -0300

    numeros decimais usando double

--- workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
+++ workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
@@ -1,19 +1,21 @@
 package cursojava.executavel;
 
-/*Aprendendo operação com números inteiros*/
+/*Resultado apresentado em numeros decimais - double*/
 
 public class PrimeiraClasse {
 
 	/* Main é um método auto executavel em Java */
 	public static void main(String[] args) {
-		
-		int nota1 = 90;
-		int nota2 = 70;
-		int nota3 = 50;
-		int nota4 = 88;
-		
-		System.out.println("Média das notas é: " + (nota1 + nota2 + nota3 + nota4) / 4);
-		
+
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		System.out.println("Média das notas é: " + mediaFinal);
+
 	}
 
 }
