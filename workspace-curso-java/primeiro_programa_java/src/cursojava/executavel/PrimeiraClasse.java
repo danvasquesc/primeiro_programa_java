382df1c062368e6cbe4d6f7c79c2cd412d8d8ba2
Dan Vasques <dan.vasques@outlook.com.br>
2024-09-24 19:18:32 -0300

    operacao com numeros inteiros

--- workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
+++ workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
@@ -1,33 +1,19 @@
 package cursojava.executavel;
 
-public class PrimeiraClasse {
+/*Aprendendo operação com números inteiros*/
 
-	/* Variável Global é acessivel a todos e seu valor compartilhado */
-	static int maiorIdadeGlobal = 30;
+public class PrimeiraClasse {
 
 	/* Main é um método auto executavel em Java */
 	public static void main(String[] args) {
-
-		/*
-		 * Variável local porque pertence somente a esse método e o valor fica dentro do
-		 * metodo que pertence
-		 */
-		int maiorIdade = 18;
-		System.out.println("Valor da variavel local = " + maiorIdade);
-
-		/* necessita colocar o metodo criado dentro do metodo executavel */
-		metodo2();
-
-	}
-
-	public static void metodo2() {
-
-		/* Variavel local do metodo */
-		int mediaAno = 50;
-
-		/* Aqui vai imprimir a variavel global */
-		System.out.println("Valor da variavel Global = " + maiorIdadeGlobal);
-
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Média das notas é: " + (nota1 + nota2 + nota3 + nota4) / 4);
+		
 	}
 
 }
