b6a6c2274920d46f70811aea078cacbdf0f0e0a2
danvasquesc <dan.vasques@outlook.com.br>
2024-10-01 18:25:25 -0300

    operacoes logicas com IF e ELSE

--- workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
+++ workspace-curso-java/primeiro_programa_java/src/cursojava/executavel/PrimeiraClasse.java
@@ -1,30 +1,26 @@
 package cursojava.executavel;
 
-/*Char representa 1 caracter, 1 letra*/
+/*Operacoes lógicas com IF e ELSE*/
 
 public class PrimeiraClasse {
 
 	/* Main é um método auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Concatenação = unir ou juntar dados */
-
+		int mediaAluno = 70;
 		String nome = "Dan";
-		String cpf = "123.456.789-10";
-		String telefone = "12 3456-7890";
-		String endereco = "Marília - SP";
-		
-		String saida = "Meu nome é " + nome + ", o CPF é " + cpf 
-				+ ", meu telefone é " + telefone + " e moro em " 
-				+ endereco;
 
-		System.out.println("Meu nome é " + nome + ", o CPF é " + cpf 
-				+ ", meu telefone é " + telefone + " e moro em " 
-				+ endereco); //pode ser feita assim
-		
-		System.out.println(saida); //ou assim (apagando o sys acima)
-		
-		
+		/*
+		 * equals() significa igual, ou seja, só será verdadeira a condição se a média
+		 * for 70 e o nome for Dan - && para aprovar condição se ambos forem verdadeiros
+		 */
+		if (mediaAluno >= 70 && nome.equals("Dan")) {
+			System.out.println("Aprovado!");
+		} else if (mediaAluno < 70) {
+			System.out.println("Reprovado!");
+		} else {
+			System.out.println("Aluno não encontrado");
+		}
 
 	}
 
